da943e3f6d08235ce77426eebba4253b2636034f
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-09-28 10:55:23 -0300

    Condição elif

--- Formacao_Python/jogo_adivinhacao/jogo_advinhacao.py
+++ Formacao_Python/jogo_adivinhacao/jogo_advinhacao.py
@@ -7,8 +7,15 @@ chute_str = input("Digite um número para tentar advinhar o numero secreto:")
 print("Você digitou ", chute_str)
 chute = int(chute_str)
 
-if(numero_secreto == chute):
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if(acertou):
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if(maior):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi menor que o número secreto")
 
