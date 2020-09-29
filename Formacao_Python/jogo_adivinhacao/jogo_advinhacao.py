06f29310e6fcec26cc2faf582544455170b217a7
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-09-29 09:48:46 -0300

    laço while

--- Formacao_Python/jogo_adivinhacao/jogo_advinhacao.py
+++ Formacao_Python/jogo_adivinhacao/jogo_advinhacao.py
@@ -3,19 +3,30 @@ print("Bem vindo ao jogo de adivinhacao!")
 print("*********************************")
 
 numero_secreto = 50
-chute_str = input("Digite um número para tentar advinhar o numero secreto:")
-print("Você digitou ", chute_str)
-chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+total_de_tentativas = 3
+rodada = 1
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto")
+while(rodada <= total_de_tentativas):
+    print("Tentativa ", rodada, " de ", total_de_tentativas)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    acertou = (chute == numero_secreto)
+    maior   = (chute > numero_secreto)
+    menor   = (chute < numero_secreto)
+
+    if(acertou):
+     print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
+
 
