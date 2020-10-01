85c16f7dee0d495fe81a7f00b2b856e3a146c820
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-10-01 10:46:09 -0300

    formatando Strings

--- Formacao_Python/jogos/jogo_forca.py
+++ Formacao_Python/jogos/jogo_forca.py
@@ -3,6 +3,25 @@ def jogar():
     print("Bem vindo ao Jogo da forca")
     print("**************************")
 
+    palavra_secreta = "banana"
+    letras_descobertas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+
+    #enquanto True and True
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_descobertas[index] = letra
+            index += 1
+        print(letras_descobertas)
+
     print("Fim do Jogo")
 
 if(__name__=="__main__"):
