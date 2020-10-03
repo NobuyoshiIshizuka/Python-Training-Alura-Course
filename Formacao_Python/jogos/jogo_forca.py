de714ad6b47fa94eb45bb27ea377a3dc2a7739e0
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-10-03 10:24:18 -0300

    Organizando o código pt-2

--- Formacao_Python/jogos/jogo_forca.py
+++ Formacao_Python/jogos/jogo_forca.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 def jogar():
 
     imprime_mensagem_abertura()
@@ -18,15 +16,10 @@ def jogar():
 
     while(not enforcou and not acertou):
 
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
+        chute = pede_chute()
 
         if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_descobertas[index] = letra
-                index += 1
+           marca_chute_correto(chute, letras_descobertas, palavra_secreta)
         else:
             erros += 1
         enforcou = erros == 6
@@ -34,13 +27,28 @@ def jogar():
         print(letras_descobertas)
 
     if(acertou):
-        print("Parabéns! Você ganhou o jogo.")
+       imprime_mensagem_vencedor()
     else:
-        print("Você perdeu!")
+        imprime_mensagem_perdedor()
 
-    print("Fim do Jogo")
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
 
 
+def imprime_mensagem_perdedor():
+    print("Você perdeu")
+
+def marca_chute_correto(chute, letras_descobertas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_descobertas[index] = letra
+        index += 1
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
 
 def imprime_mensagem_abertura():
     print("**************************")
